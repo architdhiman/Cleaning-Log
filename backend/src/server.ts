228521693949a9a6cd18a3471546129b9ce73d17
Archit <arcdhiman007@gmail.com>
2026-09-08 23:59:17 +0530

    initialize backend project

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({
+    message: "CLEEN API is running"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
